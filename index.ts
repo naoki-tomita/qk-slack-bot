b7eeda44d527b6bb22dd27b145d0d37052f6af41
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2021-05-11 10:51:35 +0900

    improve

--- index.ts
+++ index.ts
@@ -35,11 +35,16 @@ type Event = ChallengeEvent;
 
 const requests = serve({ port: parseInt(Deno.env.get("PORT") ?? "8000") });
 for await (const request of requests) {
-  const event = await parseBody(request.body, request.contentLength ?? 0);
-  console.log(event);
-  if (event.type === "url_verification") {
-    respondForChallenge(request, event);
-  } else {
-    respondForMessage(request, event);
+  try {
+    const event = await parseBody(request.body, request.contentLength ?? 0);
+    console.log(event);
+    if (event.type === "url_verification") {
+      respondForChallenge(request, event);
+    } else {
+      respondForMessage(request, event);
+    }
+  } catch (e) {
+    console.error(e);
+    request.respond({ status: 500 });
   }
 }
